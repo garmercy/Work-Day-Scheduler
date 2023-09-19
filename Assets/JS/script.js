c045d03e02bcf3a96fadaba9fa2a40a29ba3ac0a
Cristina Garcia <garmercy.mar@gmail.com>
2023-09-20 09:54:32 +1000

    changing color function

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,7 +1,7 @@
 var currentDateEl = $('#currentDay');
 var container = $(".container");
 
-//times variables that are inside the container
+//time variables that are inside the container
 var hour9 = $("#9AM");
 var hour10 = $("#10AM");
 var hour11= $("#11AM");
@@ -12,144 +12,178 @@ var hour15 = $("#15PM");
 var hour16 = $("#16PM");
 var hour17 = $("#17PM");
 
-
-
-//arrays of all the times
+//Hour array
 var hours= [hour9, hour10, hour11, hour12, hour13, hour14, hour15, hour16, hour17]
 console.log(hours);
 
 
-//Displayed at the top of the calendar the current day using moment format.
+//Display at the top of the calendar the current day using moment format.
 var todayDate = moment().format('dddd MMMM, Do');
 currentDateEl.text(todayDate);
 console.log(todayDate);
 
 
+//Function to change the colour of the timetables depending of the current time.
+$(function changeColor () {
 
-
-//This variables contains each time
-var time9 = moment().hour(9)
-var time10 = moment().hour(10);
-var time11 = moment().hour(11);
-var time12 = moment().hour(12);
-var time13 = moment().hour(13);
-var time14 = moment().hour(14);
-var time15 = moment().hour(15);
-var time16 = moment().hour(16);
-var time17 = moment().hour(17);
-var time18 = moment().hour(18);
-
-//This function is used to change the color of the timetables depending of the current time.
-$(function cahngeColor () {
-// current Time it is used to check if the current time match with the timetables
+  // Check if the current time matches with the timetables
   currentTime = moment().format("hh:mm:ss");
+  console.log(currentTime);
+
+  //These variables contains each time
+  var time9 = moment().hour(9);
+  var time10 = moment().hour(10);
+  var time11 = moment().hour(11);
+  var time12 = moment().hour(12);
+  var time13 = moment().hour(13);
+  var time14 = moment().hour(14);
+  var time15 = moment().hour(15);
+  var time16 = moment().hour(16);
+  var time17 = moment().hour(17);
+  var time18 = moment().hour(18);
+
+  console.log (time9);
  
+  //Change the colour of the background based on the current time
+  //Hour 9
   if (currentTime == time9) {
       $("#9AM").css('background-color: red');
+      console.log("working");
   }
   else if(currentTime > time9){
     $("#9AM").css('background-color: grey');
-    
+    console.log("working");
   }
   else{
     $("#9AM").css('background-color: green');
+    console.log("working");
   }
 
-  // Audit hour 10 and set color
+  //Hour 10
   if (currentTime == time10) {
       $("#10AM").css('background-color: red');
+      console.log("working");
   }
   else if (currentTime > time10) {
       $("#10AM").css('background-color: grey');
+      console.log("working");
   }
   else {
       $("#10AM").css('background-color: green');
+      console.log("working");
   }
 
-  // Audit hour 11 and set color
+  //Hour 11
   if (currentTime == time11) {
       $("#11AM").css('background-color: red');
+      console.log("working");
   }
   else if (currentTime > time11) {
       $("#11AM").css('background-color: grey');
+      console.log("working");
   }
   else {
       $("#11AM").css('background-color: green');
+      console.log("working");
   }
 
-  // Audit hour 12 and set color
+//Hour 12
   if (currentTime == time12) {
       $("#12PM").css('background-color: red');
+      console.log("working");
   }
   else if (currentTime > time12) {
       $("#12PM").css('background-color: grey');
+      console.log("working");
   }
   else {
       $("#12PM").css('background-color: green');
+      console.log("working");
   }
 
-  // Audit hour 13 and set color
+//Hour 13
   if (currentTime == time13) {
       $("#13PM").css('background-color: red');
+      console.log("working");
   }
   else if (currentTime > time13) {
       $("#13PM").css('background-color: grey');
+      console.log("working");
   }
   else {
       $("#13PM").css('background-color: green');
+      console.log("working");
   }
 
-  // Audit hour 14 and set color
+//Hour 14
   if (currentTime == time14) {
       $("#14PM").css('background-color: red');
+      console.log("working");
   }
   else if (currentTime > time14) {
       $("#14PM").css('background-color: grey');
+      console.log("working");
   }
   else {
       $("#14PM").css('background-color: green');
+      console.log("working");
   }
 
-  // Audit hour 15 and set color
+//Hour 15
   if (currentTime == time15){
       $("#15PM").css('background-color: red');
+      console.log("working");
   }
   else if (currentTime > time15) {
       $("#15PM").css('background-color: grey');
+      console.log("working");
   }
   else {
       $("#15PM").css('background-color: green');
+      console.log("working");
   }
 
-  // Audit hour 16 and set color
+//Hour 16
   if (currentTime == time16) {
       $("#16PM").css('background-color: red');
+      console.log("working");
   }
   else if (currentTime > time16) {
       $("#16PM").css('background-color: grey');
+      console.log("working");
   }
   else {
       $("#16PM").css('background-color: green');
+      console.log("working");
   }
 
-  // Audit hour 17 and set color
+//Hour 17
   if (currentTime == time17) {
       $("#17PM").css('background-color: red');
+      console.log("working");
   }
   else if (currentTime > time17) {
       $("#17PM").css('background-color: grey');
+      console.log("working");
   }
   else {
       $("#17PM").css('background-color: green');
+      console.log("working");
   };
   
 });
+ 
+console.log("after the function");
 
-console.log("despues de la funcion");
+
+
+
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function cambiandoColor () {
+$(function changeColor () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
